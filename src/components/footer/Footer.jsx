842bd9507a46cf7be5894684fc8ade5318fd1a0d
Sahil Verma <domainsahil007@gmail.com>
2021-11-11 16:25:45 +0530

    Final commit

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+import "./footer.scss";
+
+import { Link } from "react-router-dom";
+
+import bg from "../../assets/footer-bg.jpg";
+
+const Footer = () => {
+  return (
+    <div className="footer" style={{ backgroundImage: `url(${bg})` }}>
+      <div className="footer__content container">
+        <div className="footer__content__logo">
+          <div className="logo">
+            <img
+              src="https://cdn3.iconfinder.com/data/icons/vector-icons-1/96/5-512.png"
+              alt="Movie App"
+            />
+            <Link to="/">Movie App</Link>
+          </div>
+        </div>
+        <div className="footer__content__menus">
+          <div className="footer__content__menu">
+            <Link to="/">Home</Link>
+            <Link to="/">Contact us</Link>
+            <Link to="/">Term of services</Link>
+            <Link to="/">About us</Link>
+          </div>
+          <div className="footer__content__menu">
+            <Link to="/">Live</Link>
+            <Link to="/">FAQ</Link>
+            <Link to="/">Premium</Link>
+            <Link to="/">Pravacy policy</Link>
+          </div>
+          <div className="footer__content__menu">
+            <Link to="/">You must watch</Link>
+            <Link to="/">Recent release</Link>
+            <Link to="/">Top IMDB</Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
